fd9d550087fb2119bfa278f08b86c56e0fb2ced4
SP20-BCS-135 <116640955+SP20-BCS-135@users.noreply.github.com>
2023-05-20 18:55:01 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
+const ConnectToMongo = require("./database");
+const dotenv = require('dotenv');
+dotenv.config();
+
+ConnectToMongo();    
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.use(cors());
+app.use(express.json()); // parsing body
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/jobs", require("./routes/jobs"));
+
+
+
 app.listen(process.env.PORT || 3000)
